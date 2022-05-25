c47b8aab6bc0467120745aa2bcb712334876dd79
Giovanni Benussi <giovanni.benussi@usach.cl>
2022-05-25 09:44:45 -0400

    Refactor prototype approach to use classes instead and implemented a few utility methods

--- src/ExtendedResponse.test.ts
+++ src/ExtendedResponse.test.ts
@@ -1,18 +1,34 @@
 import { ExtendedResponse } from "./ExtendedResponse";
 
-test("forwards method calls to Response", () => {
-  const req = ExtendedResponse(new Response("Hello world!"));
-  expect(req.ok).toBe(true);
+//test("forwards method calls to Response", () => {
+//const req = new ExtendedResponse().send();
+//expect(req.ok).toBe(true);
+//});
+
+//test("forwards method calls to Response that access internal values", () => {
+//const req = ExtendedResponse();
+//expect(req.statusCode).toBe(500);
+//});
+
+describe("default response", () => {
+  test("returns the corresponding HTTP header", () => {
+    const res = new ExtendedResponse().json({});
+
+    expect(res.headers.get("Content-Type")).toBe("application/json");
+  });
 });
 
-test("forwards method calls to Response that access internal values", () => {
-  const req = ExtendedResponse(new Response("", { status: 500 }));
-  expect(req.status).toBe(500);
+describe("statusCode", () => {
+  test("allows to access the internal access code", () => {
+    const res = new ExtendedResponse().status(500);
+
+    expect(res.statusCode).toBe(500);
+  });
 });
 
 describe("get", () => {
   test("returns the corresponding HTTP header", () => {
-    const res = ExtendedResponse(
+    const res = new ExtendedResponse(
       new Response("", {
         headers: new Headers({
           "Content-Type": "application/json",
@@ -24,18 +40,127 @@ describe("get", () => {
   });
 });
 
+describe("set", () => {
+  test("sets the corresponding HTTP header", () => {
+    const response = new ExtendedResponse();
+
+    response.set("Content-Type", "application/json");
+
+    const res = response.send("");
+
+    expect(res.headers.get("Content-Type")).toBe("application/json");
+  });
+
+  test("accepts an object as an argument", () => {
+    const response = new ExtendedResponse();
+    response.set("Content-Type", "application/json");
+    response.set({ "Content-Type": "application/json" });
+    const res = response.send("");
+
+    expect(res.headers.get("Content-Type")).toBe("application/json");
+  });
+});
+
+describe("send", () => {
+  describe("when the parameter is a string", () => {
+    test("sends the string as the response and sets the content type to html", async () => {
+      const response = new ExtendedResponse().send("<p>hello world!</p>");
+
+      expect(await response.text()).toBe("<p>hello world!</p>");
+      expect(response.headers.get("Content-Type")).toBe(
+        "text/html;charset=UTF-8"
+      );
+    });
+
+    test("respects previously assigned content types and status code", async () => {
+      const response = new ExtendedResponse();
+      response.set("Content-Type", "text/png");
+      const res = response.status(500).send("<p>hello world!</p>");
+
+      expect(await res.text()).toBe("<p>hello world!</p>");
+      expect(res.headers.get("Content-Type")).toBe("text/png");
+      expect(res.status).toBe(500);
+    });
+  });
+
+  describe("when the parameter is an object", () => {
+    test("sends a json response", async () => {
+      const response = new ExtendedResponse().send({
+        username: "giovannibenussi",
+      });
+
+      expect(await response.text()).toBe('{"username":"giovannibenussi"}');
+      expect(response.headers.get("Content-Type")).toBe("application/json");
+    });
+  });
+
+  describe("when the parameter is a boolean value", () => {
+    test("sends a json response", async () => {
+      const response = new ExtendedResponse().send(true);
+
+      expect(await response.text()).toBe("true");
+      expect(response.headers.get("Content-Type")).toBe("application/json");
+    });
+  });
+
+  describe("when the parameter is a number", () => {
+    test("sends a json response", async () => {
+      const response = new ExtendedResponse().send(1);
+
+      expect(await response.text()).toBe("1");
+      expect(response.headers.get("Content-Type")).toBe("application/json");
+    });
+  });
+
+  describe("when the parameter is an array", () => {
+    test("sends a json response", async () => {
+      const response = new ExtendedResponse().send([1, "2"]);
+
+      expect(await response.text()).toBe('[1,"2"]');
+      expect(response.headers.get("Content-Type")).toBe("application/json");
+    });
+  });
+});
+
 describe("json", () => {
   test("encodes a json object and sets the content type to json", async () => {
-    const response = ExtendedResponse().json({ user: "giovannibenussi" });
+    const response = new ExtendedResponse().json({ user: "giovannibenussi" });
 
     expect(await response.text()).toBe('{"user":"giovannibenussi"}');
-    expect(response.get("Content-Type")).toBe("application/json");
+    expect(response.headers.get("Content-Type")).toBe("application/json");
   });
 
   test("encodes null and sets the content type to json", async () => {
-    const response = ExtendedResponse().json(null);
+    const response = new ExtendedResponse().json(null);
 
     expect(await response.text()).toBe("null");
-    expect(response.get("Content-Type")).toBe("application/json");
+    expect(response.headers.get("Content-Type")).toBe("application/json");
+  });
+});
+
+describe("status", () => {
+  test("sets a status code", async () => {
+    const response = new ExtendedResponse();
+    response.status(500);
+    expect(response.json({ user: "giovannibenussi" }).status).toBe(500);
+  });
+
+  test("encodes null and sets the content type to json", async () => {
+    const response = new ExtendedResponse().json(null);
+
+    expect(await response.text()).toBe("null");
+    expect(response.headers.get("Content-Type")).toBe("application/json");
+  });
+});
+
+describe("mime", () => {
+  test("sets the Content-Type header according to a given value", async () => {
+    const response = new ExtendedResponse();
+
+    let res = response.type("json").send("");
+    expect(res.headers.get("Content-Type")).toBe("application/json");
+
+    res = response.type(".md").send("");
+    expect(res.headers.get("Content-Type")).toBe("text/markdown");
   });
 });
--- src/ExtendedResponse.ts
+++ src/ExtendedResponse.ts
@@ -1,37 +1,83 @@
+import { lookup } from "mime-types";
+
 type ExtendedResponseType = Response & {
+  //statusCode: number;
   get: (string) => string;
   json: (object) => ExtendedResponseType;
+
+  status: (number) => ExtendedResponseType;
 };
 
-function ExtendedResponse(res = null): ExtendedResponseType {
-  if (!res) {
-    res = new Response();
+class ExtendedResponse {
+  statusCode: number;
+  headers: Headers;
+  originalResponse: Response;
+
+  constructor(originalResponse?: Response) {
+    this.originalResponse = originalResponse;
+    if (originalResponse) {
+      this.headers = originalResponse.headers;
+    } else {
+      this.headers = new Headers();
+    }
   }
-  const prototype = Object.create(Response.prototype);
 
-  prototype.get = function(name) {
+  get(name) {
     return this.headers.get(name);
-  };
+  }
 
-  prototype.set = function(name, value) {
-    return this.headers.set(name, value);
-  };
-
-  prototype.json = function(body) {
-    console.log("this.headers:", this.headers);
-    if (!this.get("Content-Type")) {
-      this.set("Content-Type", "application/json");
+  set(...params: [string, string] | [object]) {
+    if (typeof params[0] === "object") {
+      for (const [name, value] of Object.entries(params[0])) {
+        this.headers.set(name, value);
+      }
+    } else {
+      const [name, value] = params;
+      this.headers.set(name, value);
     }
 
-    return ExtendedResponse(
-      new Response(JSON.stringify(body), {
-        headers: { "Content-Type": this.get("Content-Type") },
-      })
-    );
-  };
+    return this;
+  }
 
-  Object.setPrototypeOf(res, prototype);
-  return res;
+  setIfEmpty(name, value) {
+    if (!this.get(name)) {
+      this.set(name, value);
+    }
+
+    return this;
+  }
+
+  status(value) {
+    this.statusCode = value;
+    return this;
+  }
+
+  buildResponse(body) {
+    return new Response(body, {
+      status: this.statusCode,
+      headers: this.headers,
+    });
+  }
+
+  send(body): Response {
+    switch (typeof body) {
+      case "string":
+        this.setIfEmpty("Content-Type", "text/html;charset=UTF-8");
+        return this.buildResponse(body);
+      default:
+        return this.json(body);
+    }
+  }
+
+  json(body) {
+    this.setIfEmpty("Content-Type", "application/json");
+    return this.buildResponse(JSON.stringify(body));
+  }
+
+  type(value) {
+    this.set("Content-Type", lookup(value));
+    return this;
+  }
 }
 
 export { ExtendedResponse };
