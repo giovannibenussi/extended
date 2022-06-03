78c1616ed97571fbc241c5d7978577610b69f935
Giovanni Benussi <giovanni.benussi@usach.cl>
2022-06-02 21:48:54 -0400

    Remove unused export on index file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,2 @@
 export { ExtendedRequest } from "./ExtendedRequest";
 export { ExtendedResponse } from "./ExtendedResponse";
-
-export function handleRequest(request) {
-  return new Response(`URL: ${request.url}`);
-}
